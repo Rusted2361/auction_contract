7c36ffbf75a6cd11d03fd6f950417f32d2b9fd21
Hamza <hamzasajid4040@gmail.com>
2023-08-21 12:37:17 +0500

    added boolean for auction and bid function

--- contracts/Auction.sol
+++ contracts/Auction.sol
@@ -4,13 +4,24 @@ contract Auction {
     address public seller;
     uint public latestBid;
     address public latestBidder;
+    bool public auctionFinished;
 
     constructor(uint startingPriceWei) {
         // ...
+        seller = msg.sender;
+        latestBid = startingPriceWei;
+        auctionFinished = false;
     }
 
     function bid() public {
         // ...
+         require(msg.value > latestBid, "Bid must be higher than the latest bid");
+        require(!auctionFinished, "Auction is already finished");
+        if (latestBidder != address(0)) {
+            payable(latestBidder).transfer(latestBid);
+        }
+        latestBid = msg.value;
+        latestBidder = msg.sender;
     }
 
     function finishAuction() public {
